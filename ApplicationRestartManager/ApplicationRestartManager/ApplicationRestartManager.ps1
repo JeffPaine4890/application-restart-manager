8eaa5281d59b5932451b51b026f0a5dbe14e913f
Jeffrey Paine <jpaine4890@gmail.com>
2025-12-16 22:19:23 -0500

    Moved PowerShell script into project directory
    PowerShell script will now also be placed in program directory upon building project

---
+++ ApplicationRestartManager/ApplicationRestartManager/ApplicationRestartManager.ps1
@@ -0,0 +1,21 @@
+$processesToRemove = Get-Content -Path ($PSScriptRoot + "\remove.txt")
+$processesToRemove = $processesToRemove + (Get-Content -Path ($PSScriptRoot + "\alwaysremove.txt"))
+$processesToKeep = Get-Content -Path ($PSScriptRoot + "\alwayskeep.txt")
+$runOncePath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+
+foreach ($processToRemove in $processesToRemove)
+{
+    if (Test-Path $runOncePath)
+    {
+        $valueNames = (Get-Item $runOncePath).Property
+        $pattern = [regex]::Escape($processToRemove)
+
+        foreach ($name in $valueNames | Where-Object { $_ -like 'Application Restart*' }) {
+            $data = (Get-ItemProperty -Path $runOncePath -Name $name).$name
+            $remove = $true
+            if ($data -is [string] -and $data -match $processToRemove -and $processToRemove -notin $processesToKeep) {
+                Remove-ItemProperty -Path $runOncePath -Name $name -ErrorAction SilentlyContinue
+            }
+        }
+    }
+}
